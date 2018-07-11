9c6197f4304b5207e1ccbef1aedde3258b9f0d9a
Simon St John-Green <simon.stjohn-green@softwire.com>
2018-07-11 13:36:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Counter
+=======
+
+Sample code to demonstrate generics.
+ 
+Copyright © 2018 Softwire - All Rights Reserved
---
+++ src/main/java/org/softwire/training/counter/items/Apple.java
@@ -0,0 +1,9 @@
+package org.softwire.training.counter.items;
+
+import org.softwire.training.counter.Countable;
+
+public class Apple implements Countable {
+    public int getCount() {
+        return 1;
+    }
+}
